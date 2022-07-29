2a16e607c486b04154989cff3c90865e907b925c
Dylson Oliveira <dylsonuni@gmail.com>
2022-07-29 16:25:25 +0100

    updated the JS to include both the button and the heading code

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ secondSetEl = document.getElementById("second-set");
 thirdSetEl = document.getElementById("third-set");
 
 // // Dynamically creating the question headers
-// var questionHeader = document.createElement("h2");
-// // questionHeader.dataset.number = "1";
-// // questionHeader.setAttribute = ";
-// questionHeader.textContent = "This is where the questions will go"
-// firstSetEl.appendChild(questionHeader);
-
+var questionHeader = document.createElement("h2");
+questionHeader.setAttribute("data-number", "");
+questionHeader.textContent = "This is where the questions will go"
+firstSetEl.appendChild(questionHeader);
+secondSetEl.appendChild(questionHeader);
 
 // Dynamically creating the true button
 var trueBtnEl = document.createElement("button");
@@ -17,5 +16,4 @@ trueBtnEl.setAttribute("type","button");
 trueBtnEl.setAttribute("class","btn btn-info");
 trueBtnEl.textContent="True";
 firstSetEl.appendChild(trueBtnEl);
-
 secondSetEl.appendChild(trueBtnEl);
