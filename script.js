34295deb1293d4b071f2314d476200afdb217696
Dylson Oliveira <dylsonuni@gmail.com>
2022-07-29 16:22:59 +0100

    dynamically rendered the headings and buttons via JS

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Dylson, this is working!")
+firstSetEl = document.getElementById("first-set");
+secondSetEl = document.getElementById("second-set");
+thirdSetEl = document.getElementById("third-set");
+
+// // Dynamically creating the question headers
+// var questionHeader = document.createElement("h2");
+// // questionHeader.dataset.number = "1";
+// // questionHeader.setAttribute = ";
+// questionHeader.textContent = "This is where the questions will go"
+// firstSetEl.appendChild(questionHeader);
+
+
+// Dynamically creating the true button
+var trueBtnEl = document.createElement("button");
+trueBtnEl.setAttribute("type","button");
+trueBtnEl.setAttribute("class","btn btn-info");
+trueBtnEl.textContent="True";
+firstSetEl.appendChild(trueBtnEl);
+
+secondSetEl.appendChild(trueBtnEl);
